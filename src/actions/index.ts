b0afca145a8350bce4ff601c36eb3ff9417d6672
Max Starikevich <maxim.starikevich@gmail.com>
2019-12-21 20:44:11 +0300

    Added some actions

---
+++ src/actions/index.ts
@@ -0,0 +1,9 @@
+import Telegraf, { ContextMessageUpdate } from 'telegraf'
+
+import startHandler from './start'
+import attendHandler from './attend'
+
+export const prepareActions = (bot: Telegraf<ContextMessageUpdate>) => {
+  bot.start(startHandler)
+  bot.hears(/^\+\d*$/, attendHandler)
+}
