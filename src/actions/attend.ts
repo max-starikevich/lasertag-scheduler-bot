616cbfb846a5cfd99e3c0bc1bbc20ca15a578964
Max Starikevich <maxim.starikevich@gmail.com>
2019-12-24 00:38:55 +0300

    Different message format + extended reply messages

--- src/actions/attend.ts
+++ src/actions/attend.ts
@@ -1,7 +1,5 @@
 import { ContextMessageUpdate } from 'telegraf'
-import { initSheetsClient } from '../services/sheetsClient'
-
-const getSheetsClient = initSheetsClient()
+import { updatePlayerCount } from '../services/tableManager'
 
 export default async (ctx: ContextMessageUpdate) => {
   try {
@@ -12,19 +10,45 @@ export default async (ctx: ContextMessageUpdate) => {
       return
     }
 
-    const input: string = (ctx.match && ctx.match[0]) || ''
+    const input: string = ((ctx.match && ctx.match[0]) || '').replace('я=', '')
 
     if (!input) {
-      await ctx.reply('Неизвестная ошибка.')
       return
     }
 
-    const count = input.length > 1 ? input.replace('+', '') : 1
-    const sheetsClient = await getSheetsClient
+    const [count = '0', personalWeaponsCount = '0'] = input.split('.')
 
-    await sheetsClient.set('A1', `${username}: ${count}`)
-    await ctx.reply(`@${username} добавлен в файл записи в количестве ${count}.`)
+    await updatePlayerCount(username, count, personalWeaponsCount)
+
+    const resultMessage = formatResultMessage(
+      username, 
+      parseInt(count), 
+      parseInt(personalWeaponsCount)
+    )
+
+    await ctx.reply(resultMessage)
   } catch(error) {
     console.error(error)
   }
+}
+
+const formatResultMessage = (
+  username: string,
+  count: number, 
+  personalWeaponsCount: number
+) => {
+  
+  if (count === 1) {
+    if (personalWeaponsCount > 0) {
+      return `@${username} внесен в файл записи с личкой`
+    }
+    return `@${username} внесен в файл записи`
+  } else if (count > 0) {
+    if (personalWeaponsCount > 0) {
+      return `@${username} внесен в файл записи в количестве ${count} (личка x${personalWeaponsCount})`
+    }
+    return `@${username} внесен в файл записи в количестве ${count}`
+  }
+  
+  return `@${username} выписан из файла записи`
 }
