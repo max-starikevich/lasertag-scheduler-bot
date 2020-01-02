ae1b6ec319c2c5a45507f3de8a2b5ed776e518f4
Max Starikevich <maxim.starikevich@gmail.com>
2020-01-02 17:21:18 +0300

    Improved start/help commands

--- src/actions/start.ts
+++
@@ -1,9 +0,0 @@
-import { ContextMessageUpdate } from 'telegraf'
-
-export default async (ctx: ContextMessageUpdate): Promise<void> => {
-  try {
-    await ctx.reply('👋')
-  } catch(error) {
-    console.error(error)
-  }
-}
