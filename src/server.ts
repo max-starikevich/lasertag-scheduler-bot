c222c45f2e3f20dcbf505be44667a79877a6cbbf
Max Starikevich <maxim.starikevich@gmail.com>
2020-01-05 00:59:26 +0300

    Adjusted bot preparation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import { prepareBot } from './actions'
+import { checkEnvironment } from './environment'
 
-prepareBot()
-  .launch()
+checkEnvironment()
+  .then(prepareBot)
+  .then(bot => bot.launch())
   .then(() => console.log('🚀 Bot has started successfully!'))
   .catch(error => {
-    console.error('❌ Bot has failed to start.', error)
+    console.error('❌ Bot has failed to start.', error.message)
     process.exit(1)
   })
-
+  
 process.on('unhandledRejection', reason => {
   console.error('❌ Unhandled Rejection.', reason)
 })
+  
