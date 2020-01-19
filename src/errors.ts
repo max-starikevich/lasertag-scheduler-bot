b350a0eae82fb8bce4feb23bba39ac93f160dbaa
Max Starikevich <maxim.starikevich@gmail.com>
2020-01-19 16:13:28 +0300

    Added error handling module

---
+++ src/errors.ts
@@ -0,0 +1,12 @@
+export const handleActionError = (error: Error) => {
+  console.error('❌ Action error.', error)
+}
+
+export const handleStartupError = (error: Error) => {
+  console.error('❌ Startup error.', error.message)
+  process.exit(1)
+}
+
+export const handleUnexpectedRejection = (error: any) => {
+  console.error('❌ Unexpected rejection.', error)
+}
